6a201365103e8e7a236d8a9629eca18239116309
JoaoJoseFaria <jfaria.gmr@gmail.com>
2014-11-16 23:04:30 +0000

    Segunda parte

--- app/routes.php
+++ app/routes.php
@@ -15,11 +15,17 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
-Route::resource('projects', 'ProjectsController');
-Route::bind('projects', function($value, $route) {
-    return Project::whereSlug($value)->first();
-});
-Route::resource('projects.tasks', 'TasksController');
+
+// Provide controller methods with object instead of ID
+Route::model('tasks', 'Task');
+Route::model('projects', 'Project');
+// Use slugs rather than IDs in URLs
 Route::bind('tasks', function($value, $route) {
     return Task::whereSlug($value)->first();
 });
+Route::bind('projects', function($value, $route) {
+    return Project::whereSlug($value)->first();
+});
+
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
